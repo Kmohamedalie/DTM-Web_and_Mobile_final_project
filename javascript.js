5b5fd4e78b8f05321f9c51b619c6ff2615dacc41
Mohamed Alie Kamara <63104472+Kmohamedalie@users.noreply.github.com>
2023-02-19 13:40:03 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -27,29 +27,3 @@ function closeNav() {
         thissound.pause();
         thissound.currentTime = 0;
       }
-
-<!--Javascript code top button-->
-// Get the button
-let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-          scrollFunction();
-        };
-
-function scrollFunction() {
-          if (
-            document.body.scrollTop > 20 ||
-            document.documentElement.scrollTop > 20
-          ) {
-            mybutton.style.display = "block";
-          } else {
-            mybutton.style.display = "none";
-          }
-        }
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-          document.body.scrollTop = 0;
-          document.documentElement.scrollTop = 0;
-        }
